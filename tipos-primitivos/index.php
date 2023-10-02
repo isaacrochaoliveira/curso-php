557fdef69eb65f66f9e93f94340afb3af6d160b1
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-01 22:27:31 -0300

    Tipos Primitivos

---
+++ tipos-primitivos/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de Tipos Primitivos</h1>
+    <?php 
+        // 0x = HexaDecimal
+        // 0b = Binário
+        // 0 = Octal
+
+        //$num = 010;
+        //echo "O valor da variável é $num";
+
+        //$v = "Gustavo";
+        //var_dump($v);
+
+        //$num = 3e2; // 3 * 10 ^ 2
+        //$num = (double) 3e2; //(Integer ou Int)  
+        //echo $num;
+        //var_dump($num); 
+        
+        //$casado = true;
+        //var_dump($casado);
+        //print "O valor do casado é $casado";
+
+        // Compostos
+
+        //Array
+        //$vet = [6, 2.5, "Maria", 3, false];
+        //echo "O vetor é $vet";
+        //var_dump($vet);
+
+        //Object
+        //class Pessoa {
+        //    private string $nome;
+        //}
+        //$p = new Pessoa;
+        //var_dump($p);
+    ?>
+</body>
+</html>
