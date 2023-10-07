10698c3969df9fdd9b579b6ca340d0498e2ef763
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-07 20:41:54 -0300

    Anatomia de Divisão

--- desafios/ex05-anatomia-de-divicao/index.php
+++ desafios/ex05-anatomia-de-divicao/index.php
@@ -5,32 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anatomia de Uma Divição</title>
     <link rel="stylesheet" href="../style.css">
-    <style>
-        .text-align {
-            text-align: center;
-        }
-        .d-flex {
-            display: flex;
-        }
-        .justify-content-around {
-            justify-content: space-around;
-        }
-        .fs-40 {
-            font-size: 25px;
-        }
-        .border-right {
-            border-right: 3px solid #000;
-        }
-        .border-top {
-            border-top: 3px solid #000;
-        }
-        .pr-5 {
-            padding-right: 2rem;
-        }
-        .mb-0 {
-            margin-bottom: 0px;
-        }
-    </style>
 </head>
 <body>
     <?php 
@@ -54,20 +28,16 @@
 
     <section>
         <h2>Estrutura da Divisão</h2>
-        <?php 
-            echo <<< HTML
-                <div class="d-flex justify-content-around fs-40">
-                    <div>
-                        <h1 class='border-right pr-5 mb-0'>$dividendo</h1>
-                        <h1 class="border-right">$resto</h1>
-                    </div>
-                    <div>
-                        <h1>$divisor</h1>
-                        <h1 class="border-top">$valor</h1>
-                    </div>
-                </div>
-            HTML;
-        ?>
+        <table class="divisao">
+            <tr>
+                <td><?= $dividendo ?></td>
+                <td><?= $divisor ?></td>
+            </tr>
+            <tr>
+                <td><?= $resto ?></td>
+                <td><?= $valor ?></td>
+            </tr>
+        </table>
     </section>
 
 </body>
