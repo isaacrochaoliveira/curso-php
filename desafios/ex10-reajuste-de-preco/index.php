d90ea784c9e6ceda01cde0c88b15b9cc6bef6fd2
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-07 18:04:01 -0300

    Update index.php

--- desafios/ex10-reajuste-de-preco/index.php
+++ desafios/ex10-reajuste-de-preco/index.php
@@ -8,8 +8,13 @@
 </head>
 <body>
     <?php 
+        
         $preco = $_POST['preco'] ?? 1000;
         $reajuste = $_POST['reajuste'] ?? 10;
+        
+        $r = $preco + ($preco * $reajuste / 100);
+        
+        $formato = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
     ?>
     <header>
         <h1>Reajustadpr de Preços</h1>
@@ -18,8 +23,17 @@
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
             <label for="preco">Preço do Produto</label>
             <input type="number" name="preco" id="preco" value="<?= $preco ?>" step="0.01">
-            <label for=""></label>
+            <label for="reajuste">Qual será o percentual do reajuste?</label>
+            <input type="number" name="reajuste" id="reajuste" step="0.01" value="<?= $reajuste ?>">
+            <input type="submit" value="Reajustar">
         </form>
     </main>
+    <section>
+        <h2>Resultado do Reajuste</h2>
+        <?php 
+
+            echo "O produto que custava " . numfmt_format_currency($formato, $preco, 'BRL') . ", com <strong>$reajuste% de aumento</strong> vai passar a custar <strong>" . numfmt_format_currency($formato, $r, "BRL");
+        ?>
+    </section>
 </body>
 </html>
