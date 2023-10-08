2fd74c1a56818ac6590f32e7f31d723ca3081b7f
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-07 22:05:24 -0300

    Iguais São?

---
+++ pratica/ex03-somar-se-iguais/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>São Iguais?</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <?php 
+        $a = $_GET['a'] ?? 0;
+        $b = $_GET['b'] ?? 1;
+    ?>
+    <main>
+        <h1>São Iguais?</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="a">Informe o valor de A:</label>
+            <input type="number" name="a" id="a" value="<?= $a ?>">
+            <label for="b">Informe o valor de B:</label>
+            <input type="number" name="b" id="b" value="<?= $b ?>">
+            <input type="submit" value="OK">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado</h2>
+        <p>
+            <?php 
+                if ($a == $b) {
+                    echo "A e B são Iguais, portanto $a + $b = ". $a + $b;
+                } else {
+                    echo "A e B são diferentes";
+                }
+            ?>
+        </p>
+    </section>
+</body>
+</html>
